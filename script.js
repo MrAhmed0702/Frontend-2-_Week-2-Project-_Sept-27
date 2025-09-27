9c2e0e3fa72e36db968e277ecf4abd674143cf56
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-28 00:11:48 +0530

    updated by securing the submit button

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ function validateEmail() {
         eError.textContent = 'All good to go!';
         eError.style.color = 'green';
         eError.style.fontSize = '14px';
+        return true;
     } else {
         eError.textContent = 'Make sure email is more than 3 characters and has "@" and "."';
         eError.style.color = 'red';
         eError.style.fontSize = '14px';
+        return false;
     }
 }
 
@@ -27,25 +29,46 @@ function validatePassword() {
         pError.textContent = 'All good to go!';
         pError.style.color = 'green';
         pError.style.fontSize = '14px';
+        return true;
     } else {
         pError.textContent = 'Make sure password is more than 8 characters.';
         pError.style.color = 'red';
         pError.style.fontSize = '14px';
+        return false;
     }
 }
 
-eInput.addEventListener('input', validateEmail);
-pInput.addEventListener('input', validatePassword);
+eInput.addEventListener('input', updateSubmitButtonState);
+pInput.addEventListener('input', updateSubmitButtonState);
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    const isEValid = validateEmail();
+    const isPValid = validatePassword();
+
+    if (!isEValid || !isPValid) {
+        return;
+    }
+
     const confirmed = confirm('Are you sure you want to submit the form?');
     if (confirmed) {
         alert('Successful signup!');
+        eInput.value = '';
+        pInput.value = '';
+        eError.textContent = '';
+        pError.textContent = '';
+        submitButton.disabled = true;
     } else {
         eInput.value = '';
         pInput.value = '';
         eError.textContent = '';
         pError.textContent = '';
+        submitButton.disabled = true;
     }
 });
+
+function updateSubmitButtonState() {
+    const isEValid = validateEmail();
+    const isPValid = validatePassword();
+    submitButton.disabled = !(isEValid && isPValid);
+}
